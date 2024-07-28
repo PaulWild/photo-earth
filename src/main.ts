a6ebe93dbfd4df20c7029087ec7796827113eb8e
Paul Wild <paulwild@gmail.com>
2024-07-28 17:59:55 +0100

    bloody typescript

--- src/main.ts
+++ src/main.ts
@@ -28,14 +28,18 @@ drawHexes();
 var drag = d3.drag().on("drag", dragged);
 var zoom = d3.zoom().on("zoom", zoomed);
 
+// @ts-expect-error
 svg.call(drag);
+// @ts-expect-error
 svg.call(zoom);
 
 function drawMap() {
   g.selectAll("path")
+    // @ts-expect-error
     .data(worldMap.features)
     .enter()
     .append("path")
+    // @ts-expect-error
     .attr("d", path);
 }
 
@@ -69,29 +73,30 @@ function drawHexes() {
     .attr("height", 1);
 }
 
-function dragged(event) {
-  const max = 180;
-  const min = -180;
-
+function dragged(event: { dx: number; dy: number }) {
   var dx = event.dx / 50;
   var dy = event.dy / 50;
 
   var currentCenter = projection.center();
   projection.center([currentCenter[0] - dx, currentCenter[1] + dy]);
+  // @ts-expect-error
   g.selectAll("path").attr("d", path);
+  // @ts-expect-error
   g2.selectAll("path").attr("d", path);
 }
 
-function zoomed(event) {
+function zoomed(event: { sourceEvent: { wheelDelta: number } }) {
   projection.scale(
     bind(projection.scale() + event.sourceEvent.wheelDelta * 10, 100, 10000)
   );
 
+  // @ts-expect-error
   g.selectAll("path").attr("d", path);
+  // @ts-expect-error
   g2.selectAll("path").attr("d", path);
 }
 
-function bind(value, min, max) {
+function bind(value: number, min: number, max: number) {
   if (value < min) {
     return min;
   }
